d33b8ed38bbef2b85a56c61cb1adb20ba750e132
playerSuNn <nguyenlelenhutnhut@gmail.com>
2021-10-13 20:33:36 +0700

    nộp bổ xung tuần 5

--- src/com/nhutnl2008110101/lab5/testAlldrive.java
+++ src/com/nhutnl2008110101/lab5/testAlldrive.java
@@ -3,19 +3,22 @@ package com.nhutnl2008110101.lab5;
 public class testAlldrive {
     public static void main(String[] args) {
         //Sách 
-        /*book sach = new book();
+        book sach = new book();
         sach.enter();
         sach.print();
         //Nhân Viên
         customer nv = new customer();
         nv.enterCus();
         nv.printCus();
+        //Xe
         car xe = new car();
         xe.enterCar();
         xe.printCar();
+        //Tài khoản
         account tk = new account();
         tk.enterAcc();
-        tk.printAcc();*/
+        tk.printAcc();
+        //Máy tính
         computer pc = new computer();
         pc.enterPC();
         pc.printPC();
