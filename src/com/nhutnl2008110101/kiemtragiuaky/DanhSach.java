c681d61281501ea4bcd6a2cf0328b9171df3a0be
playerSuNn <nguyenlelenhutnhut@gmail.com>
2021-11-04 08:44:43 +0700

    nộp bổ sung lần 1

---
+++ src/com/nhutnl2008110101/kiemtragiuaky/DanhSach.java
@@ -0,0 +1,18 @@
+package com.nhutnl2008110101.kiemtragiuaky;
+
+import java.util.Scanner;
+
+public class DanhSach {
+    static Scanner sc = new Scanner(System.in);
+
+    DanhSach(){}
+
+    protected void themNguoi(){
+        System.out.println("Nhập thông tin ");
+    }
+
+    protected void inDanhSach(){
+
+    }
+    
+}
---
+++ src/com/nhutnl2008110101/kiemtragiuaky/DanhSachQL.java
@@ -0,0 +1,9 @@
+package com.nhutnl2008110101.kiemtragiuaky;
+
+import java.util.ArrayList;
+
+public class DanhSachQL {
+    ArrayList<String> danhSach = new ArrayList<>();
+    
+    
+}
