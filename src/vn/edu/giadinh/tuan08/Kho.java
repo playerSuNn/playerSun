7941da42b4a85f9425e19c2d1b517ebe6258f71c
playerSuNn <nguyenlelenhutnhut@gmail.com>
2021-11-19 10:14:05 +0700

    thử kho mới

--- src/vn/edu/giadinh/tuan08/Kho.java
+++ src/vn/edu/giadinh/tuan08/Kho.java
@@ -3,6 +3,7 @@ package vn.edu.giadinh.tuan08;
 public class Kho {
     public static void main(String[] args) {
         System.out.println("In ra mot cai gi do");
+        System.out.println("them vào vài thứ để thử kho mới ");
     }
     
 }
